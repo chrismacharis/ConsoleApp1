cf5c4fae52a7be3232d3cada680575ebd24dfb33
User <User@DESKTOP-06G2A14>
2025-09-24 18:40:23 +0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 21);
+            int userInput;
+          
+            do
+            {
+                Console.WriteLine("This is a number guessing game.\nEnter a number from 1-20:");
+                userInput = Convert.ToInt32(Console.ReadLine());
+
+                if (userInput == randomNumber)
+                {
+                    Console.WriteLine("You guessed it right!");
+                }
+                else if (userInput < randomNumber)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+            }
+            while (userInput != randomNumber);
+            
+        }
+    }
+}
